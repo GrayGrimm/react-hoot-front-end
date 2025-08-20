9cd45b702fe938bc43e9ba69239f8dcf0531b579
guyera <thegraygrimm@graymansurvivaltech.com>
2025-08-20 13:31:00 -0400

    Hoot deatail component complete

--- src/services/hootService.js
+++ src/services/hootService.js
@@ -1,5 +1,7 @@
 const BASE_URL = `${import.meta.env.VITE_BACK_END_SERVER_URL}/hoots`;
 
+
+//Get list of hoots
 const index = async () => {
     try {
         const res = await fetch(BASE_URL, {
@@ -11,6 +13,20 @@ const index = async () => {
     }
 };
 
+//get detasils for clicked hoot (SHOW)
+
+const show = async (hootId) => {
+    try {
+        const res = await fetch(`${BASE_URL}/${hootId}`, {
+            headers: { Authorization: `Bearer ${localStorage.getItem('token')}` },
+        });
+        return res.json();
+    } catch (error) {
+        console.log(error);
+    }
+};
+
 export {
     index,
+    show,
 }
